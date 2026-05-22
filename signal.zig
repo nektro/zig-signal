578b7a2ef6f1e0f25def417ad098c0d90648242b
Meghan Denny <hello@nektro.net>
2026-05-22 13:56:15 -0700

    lowercase c callconv

--- signal.zig
+++ signal.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const __sighandler_t = ?*const fn (c_int) callconv(.C) void;
+const __sighandler_t = ?*const fn (c_int) callconv(.c) void;
 extern fn signal(__sig: c_int, __handler: __sighandler_t) __sighandler_t;
 
 pub fn listenFor(sig: c_int, comptime f: fn () void) void {
@@ -9,7 +9,7 @@ pub fn listenFor(sig: c_int, comptime f: fn () void) void {
 
 fn Handler(comptime f: fn () void) type {
     return struct {
-        pub fn handle(s: c_int) callconv(.C) void {
+        pub fn handle(s: c_int) callconv(.c) void {
             std.debug.print("\n", .{});
             std.log.info("caught signal: {d}", .{s});
             f();
